9c12c6542f6d4e4b10ab3cf51ea740598181b946
Vitor <vitorcooutinho@gmail.com>
2024-11-16 18:49:08 -0300

    feat: first frontend layout

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/sobre', (req, res) => {
+    res.render('sobre');
+});
+
+app.get('/reservas', (req, res) => {
+    res.render('reservas');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/cadastro', (req, res) => {
+    res.render('cadastro');
+});
+
+app.get('/carrinho', (req, res) => {
+    res.render('carrinho');
+});
+
+app.get('/quarto', (req, res) => {
+    res.render('quarto');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
